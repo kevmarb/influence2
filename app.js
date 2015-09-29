93aa17927190018d164f601083b854f9496e37e2
kevin marburger <kevin.marburger26@gmail.com>
2015-09-29 15:02:58 +0200

    Influence Prof

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+var express = require('express');
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('choice', function(what){
+    console.log('chosen: ' + what);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
